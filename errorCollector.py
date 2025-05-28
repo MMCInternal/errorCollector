44432935069d0a6f0f9221cbb081c815cd13ae28
Dylan <popedyit@gmail.com>
2025-05-28 17:08:27 -0400

    Add errorCollector.py for system information collection
    This new file introduces a script that collects system information including CPU, memory, and GPU details using the psutil, GPUtil, and wmi libraries. The collected data is timestamped and can be filtered based on specific keys. It will eventually be upated to collect error info.

---
+++ errorCollector.py
@@ -0,0 +1,22 @@
+import psutil
+import GPUtil
+import wmi
+import platform
+from datetime import datetime
+
+def collect_system_info(x = '*'):
+    computer = wmi.WMI()
+    system_info = {}
+    system_info['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    system_info['platform'] = platform.system()
+    system_info['platform_version'] = platform.version()
+    system_info['Cpu Name'] = computer.win32_processor()[0].Name
+    system_info['Memory'] = computer.win32_physicalmemory()
+    system_info['Gpu Name'] = computer.win32_videocontroller()[0].Name
+    if x == '*':
+        return system_info
+    else:
+        return system_info[x]
+
+
+print(collect_system_info())
